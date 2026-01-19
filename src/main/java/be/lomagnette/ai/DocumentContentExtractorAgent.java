9b94cb1807901f547b5dbcce5e45fe67084c397e
Loic Magnette <loic.magnette@gmail.com>
2026-01-19 13:15:19 +0100

    feat: update vision model in Ollama config and enhance `DocumentContentExtractorAgent`
    - Replace `deepseek-ocr` with `qwen3-vl` in Ollama vision model configuration.
    - Add `NoChatMemoryProviderSupplier` to `DocumentContentExtractorAgent` for improved agent definition.

--- src/main/java/be/lomagnette/ai/DocumentContentExtractorAgent.java
+++ src/main/java/be/lomagnette/ai/DocumentContentExtractorAgent.java
@@ -5,13 +5,11 @@ import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 
-@RegisterAiService(modelName = "gpt4")
+@RegisterAiService(modelName = "gpt4", chatMemoryProviderSupplier = RegisterAiService.NoChatMemoryProviderSupplier.class)
 @SystemMessage("Extract and summarize text from the provided image.")
 public interface DocumentContentExtractorAgent {
     @UserMessage("""
             Extract the content of this document.
-            You should collect all the data that might concern dog adoption,
-            dog adoption permit.
            """)
     String extractReceiptData(Image image);
 }
